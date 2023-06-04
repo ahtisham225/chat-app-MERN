6d5a12a075f0af937865fcf208a9ef9b5736ccdc
Ahtisham <ahtisham225ali@gmail.com>
2023-06-04 19:56:26 +0500

    Deploying

--- src/Axios.js
+++ src/Axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:9000',
+    baseURL: 'https://chat-app-ahtisham.herokuapp.com/',
 });
 
 export default instance;
